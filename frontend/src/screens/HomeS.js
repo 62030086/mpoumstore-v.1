ffac99e80a336ff218db961565c6f5fe100251c3
Kany_SB <62030086@kmitl.ac.th>
2021-12-06 13:37:51 +0700

    OK

---
+++ frontend/src/screens/HomeS.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css/HomeS.css'
+
+const HomeS = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HomeS
---
+++ frontend/src/screens/ProductS.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css/ProductS.css'
+
+const ProductS = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProductS
